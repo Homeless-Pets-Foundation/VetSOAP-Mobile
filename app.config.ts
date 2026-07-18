dbbca6b4033138d1aa51fcad1503ce0297b9d3dd
jaxnnux <jaxnnux@gmail.com>
2026-07-17 23:10:01 -0400

    chore: bump mobile release to 1.13.13 (#148)
    Co-authored-by: jaxnnux <185745515+jaxnnux@users.noreply.github.com>

--- app.config.ts
+++ app.config.ts
@@ -134,7 +134,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     name: IS_LOCAL_TEST ? 'Captivet Local' : 'Captivet',
     slug: 'vetsoap-mobile',
     scheme: IS_LOCAL_TEST ? 'captivet-local' : 'captivet',
-    version: '1.13.11',
+    version: '1.13.13',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'automatic',
